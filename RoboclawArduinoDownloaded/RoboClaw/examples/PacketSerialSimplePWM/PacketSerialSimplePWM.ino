1bbf2f0bbc565d5626c87b83520d6800e2377a79
Elaine Lowinger <41348398+nerdyneptunian@users.noreply.github.com>
2019-11-21 09:05:51 -0800

    Added RoboClaw example codes
    Downloaded this RoboClaw code to interface with Arduino. Most important thing in this is the .cpp and .h files for ease of functions.

---
+++ RoboclawArduinoDownloaded/RoboClaw/examples/PacketSerialSimplePWM/PacketSerialSimplePWM.ino
@@ -0,0 +1,22 @@
+ //See BareMinimum example for a list of library functions
+
+//Includes required to use Roboclaw library
+#include <SoftwareSerial.h>
+#include "RoboClaw.h"
+
+//See limitations of Arduino SoftwareSerial
+SoftwareSerial serial(10,11);	
+RoboClaw roboclaw(&serial,10000);
+
+#define address 0x80
+
+void setup() {
+  //Open roboclaw serial ports
+  roboclaw.begin(38400);
+}
+
+void loop() {
+  roboclaw.ForwardM1(address,12); //start Motor1 forward at half speed
+  roboclaw.BackwardM2(address, 12); 
+  delay(2000);
+ }
