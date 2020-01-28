52facf0551839aa5f1e8886ae88dc685bc44f894
mjbrown2017 <mjbrown1123@gmail.com>
2020-01-27 22:42:31 -0800

    centerbot remote and rudder functionality

---
+++ centerbot/centerbot-rudder/centerbot-rudder.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+#define RUDDER_PIN 3
+
+Servo rudder;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  rudder.attach(RUDDER_PIN);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  delay(2000);
+
+  rudder.write(100);
+
+  delay(2000);
+
+  rudder.write(0);
+
+}
